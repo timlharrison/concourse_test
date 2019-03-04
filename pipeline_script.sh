af687a3af3a02a2b0931f85e22ac881a7ec30934
timlharrison <33101253+timlharrison@users.noreply.github.com>
2019-03-04 15:43:32 -0500

    Update pipeline_script.sh

--- pipeline_script.sh
+++ pipeline_script.sh
@@ -5,11 +5,11 @@ apt-get -qq update
 echo "installing curl"
 apt-get install curl -qq > /dev/null
 echo "downloading om cli"
-curl -LO https://github.com/pivotal-cf/om/releases/download/0.53.0/om-linux
+#curl -LO https://github.com/pivotal-cf/om/releases/download/0.53.0/om-linux
 echo "setting permissions on om cli"
-chmod +x ./om-linux
+#chmod +x ./om-linux
 ls -l
 echo "show man page"
-./om-linux
+om-linux
 echo "curl google.com"
 curl -I www.google.com
