9da2cf47e338767762f8dad878c913cca7bb81f2
timlharrison <33101253+timlharrison@users.noreply.github.com>
2019-03-04 15:44:27 -0500

    Update pipeline_script.sh

--- pipeline_script.sh
+++ pipeline_script.sh
@@ -4,9 +4,9 @@ echo "running apt-get update"
 apt-get -qq update
 echo "installing curl"
 apt-get install curl -qq > /dev/null
-echo "downloading om cli"
+#echo "downloading om cli"
 #curl -LO https://github.com/pivotal-cf/om/releases/download/0.53.0/om-linux
-echo "setting permissions on om cli"
+#echo "setting permissions on om cli"
 #chmod +x ./om-linux
 ls -l
 echo "show man page"
